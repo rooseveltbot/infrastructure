6bd01a5d2b93c0e428f356a4b527a0c2505dbf55
Jelle Maas <typiqally@gmail.com>
2022-08-11 00:02:14 +0200

    Add entity repository interfaces

---
+++ Roosevelt.Common.Persistence/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Roosevelt.Common.Persistence;
+
+public interface IRepository<TEntity> : IReadOnlyRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    TEntity Create(TEntity entity);
+    Task<TEntity> CreateAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(object id);
+    void Delete(TEntity entity);
+    Task DeleteAsync(object id);
+    int SaveChanges();
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = new());
+}
