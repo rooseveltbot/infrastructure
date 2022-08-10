cf80e9bef4d3ab20046fd091776639f44fbf8975
Jelle Maas <typiqally@gmail.com>
2022-08-11 00:03:02 +0200

    Add Entity Framework data context interface

---
+++ Roosevelt.Common.Persistence.EntityFramework/IEntityFrameworkContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Roosevelt.Common.Persistence.EntityFramework;
+
+public interface IEntityFrameworkContext : IDataContext
+{
+    DatabaseFacade Database { get; }
+    DbSet<TEntity> Set<TEntity>() where TEntity : class;
+    DbSet<TEntity> Set<TEntity>(string name) where TEntity : class;
+    EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+}
